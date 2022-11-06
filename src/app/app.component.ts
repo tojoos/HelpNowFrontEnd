29f7869034db1dbd19880586472bc34937591f14
ToJoos <joosx2000@gmail.com>
2022-11-06 23:23:29 +0100

    Added router and navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Employee} from './employee';
-import {EmployeeService} from "./employee.service";
-import {HttpErrorResponse} from "@angular/common/http";
-import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,100 +6,8 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public employees: Employee[] = [];
-  public editEmployee!: Employee | undefined;
-  public deleteEmployee!: Employee | undefined;
-
-  constructor(private employeeService: EmployeeService) {
-  }
 
   ngOnInit() {
-    this.getEmployees();
   }
 
-  public getEmployees(): void {
-    this.employeeService.getEmployees().subscribe({
-      next: (response: Employee[]) => {
-        this.employees = response;
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    });
-  }
-
-  public onAddEmployee(addForm: NgForm): void {
-    document.getElementById('add-employee-form')!.click();
-    this.employeeService.addEmployee(addForm.value).subscribe({
-      next: (response: Employee) => {
-        console.log(response);
-        this.getEmployees();
-        addForm.reset();
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-        addForm.reset();
-      }
-    });
-  }
-
-  public onUpdateEmployee(employee: Employee): void {
-    document.getElementById('edit-employee-form')!.click();
-    this.employeeService.updateEmployee(employee).subscribe({
-      next: (response: Employee) => {
-        console.log(response);
-        this.getEmployees();
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    });
-  }
-
-  public onDeleteEmployee(employeeId: number): void {
-    this.employeeService.deleteEmployee(employeeId).subscribe({
-      next: (response: void) => {
-        console.log(response);
-        this.getEmployees();
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    });
-  }
-
-  public onOpenModal(mode: string, employee?: Employee): void {
-    const container = document.getElementById('main-container');
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');
-    if (mode === 'add') {
-      button.setAttribute('data-target', '#addEmployeeModal');
-    } else if (mode === 'edit') {
-      this.editEmployee = employee!;
-      button.setAttribute('data-target', '#editEmployeeModal');
-    } else if (mode === 'delete') {
-      this.deleteEmployee = employee!;
-      button.setAttribute('data-target', '#deleteEmployeeModal');
-    }
-    container!.appendChild(button);
-    button.click();
-  }
-
-  public searchEmployees(key: string): void {
-    const results: Employee[] = [];
-    for (const employee of this.employees) {
-      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
-        || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
-        || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
-        || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
-        results.push(employee);
-      }
-    }
-    this.employees = results;
-    if (results.length === 0 || !key) {
-      this.getEmployees();
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from '@angular/forms';
+import { EmployeesComponent } from './employees/employees.component';
+import { FundraisesComponent } from './fundraises/fundraises.component';
+import { OrganizationsComponent } from './organizations/organizations.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    FundraisesComponent,
+    OrganizationsComponent,
+    ContactComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    AnnouncementsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
