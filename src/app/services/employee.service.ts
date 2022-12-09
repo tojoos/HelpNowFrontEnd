38ec0ae0c261cdc76519e98575a3dbe708ef2516
ToJoos <joosx2000@gmail.com>
2022-12-09 23:17:53 +0100

    Moved employees page to separate component

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,8 @@
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Employee } from "./employee";
-import { environment } from "../environments/environment";
+import { Employee } from "../model/employee";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
