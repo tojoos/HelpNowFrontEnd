880bab4de318661b60560e7dcbe05b334dde468c
ToJoos <joosx2000@gmail.com>
2022-12-24 13:58:10 +0100

    Added footer to the webservice. Closes #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import { Announcement } from './model/announcement';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AnnouncementService } from './services/announcement.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +9,27 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  ngOnInit() {
+  public latestAnnouncements: Announcement[] = [];
+  
+  constructor(private announcementService: AnnouncementService) {
   }
 
+  ngOnInit() {
+    this.getAnnouncements();
+  }
+
+  public getAnnouncements(): void {
+    this.announcementService.getAnnouncements().subscribe({
+      next: (response: Announcement[]) => {
+        var announcement: Announcement[]
+        announcement = response;
+        announcement.sort((f1, f2) => new Date(f2.creationDateTime).getTime() - new Date(f1.creationDateTime).getTime());
+        announcement.sort((f1, f2) => f2.status === 'closed' ? -1 : 1)
+        this.latestAnnouncements = announcement.slice(0,2)
+        },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    });
+  }
 }
